4673be45daaac8215c3d58a894f50fc06d04c98c
Vladislav <v.kirbaba@gmail.com>
2015-08-04 12:42:45 +0300

    WP OK!

--- functions.php
+++ functions.php
@@ -1,25 +1,30 @@
 <?php
 
-define('TM_DIR', get_template_directory(__FILE__));
-define('TM_URL', get_template_directory_uri(__FILE__));
-
-require_once TM_DIR.'/lib/parser.php';
-
-function add_style(){
-    wp_enqueue_style( 'my-bootstrap-extension', get_template_directory_uri() . '/css/bootstrap.css', array(), '1');
-    wp_enqueue_style( 'my-styles', get_template_directory_uri() . '/css/style.css', array('my-bootstrap-extension'), '1');
-    wp_enqueue_style( 'my-sass', get_template_directory_uri() . '/sass/style.css', array('my-bootstrap-extension'), '1');
+function add_style(){    
+    wp_enqueue_style( 'my-styles', get_template_directory_uri() . '/css/style.css', array(), '1');
+    wp_enqueue_style( 'my-sass', get_template_directory_uri() . '/sass/style.css', array(), '1');
     wp_enqueue_style( 'fotorama', get_template_directory_uri() . '/css/fotorama.css', array('my-bootstrap-extension'), '1');
 }
 
-function add_script(){
-    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-2.1.3.min.js', array(), '1');
-    wp_enqueue_script( 'jq', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', array(), '1');
-    wp_enqueue_script( 'my-bootstrap-extension', get_template_directory_uri() . '/js/bootstrap.js', array(), '1');
-    wp_enqueue_script( 'my-script', get_template_directory_uri() . '/js/script.js', array(), '1');
-    wp_enqueue_script( 'fotorama-js', get_template_directory_uri() . '/js/fotorama.js', array(), '1');
-
+function add_script(){   
+    wp_enqueue_script( 'jq', 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js', array(), '1');
+    wp_enqueue_script( 'nav', get_template_directory_uri() . '/js/nav.js', array(), '1', true);
     
+    wp_enqueue_script( 'prices', get_template_directory_uri() . '/js/prices.js', array(), '1', true);
+    wp_enqueue_script( 'moreworks', get_template_directory_uri() . '/js/moreworks.js', array(), '1', true);
+    wp_enqueue_script( 'popup', get_template_directory_uri() . '/js/popup.js', array(), '1', true);
+    wp_enqueue_script( 'form', get_template_directory_uri() . '/js/form.js', array(), '1', true);
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array(), '1', true);
+        wp_localize_script('my-script', 'myajax',
+        array(
+            'url' => admin_url('admin-ajax.php')
+        )
+    );
+    wp_localize_script( 'nav', 'img',
+    array(
+        'url' => get_template_directory_uri(),
+        'act' => admin_url('admin-ajax.php')
+    ));
 }
 
 add_action( 'wp_enqueue_scripts', 'add_style' );
